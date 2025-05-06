9a7d4deed42c6c3bc6342c8710a4ac3ed3271eaa
Rahul Rohilla <rahul.rohilla1081@gmail.com>
2025-05-06 23:19:26 +0530

    Minor changes

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ import {name as appName} from './app.json';
 import PushNotification from 'react-native-push-notification';
 import BackgroundFetch from 'react-native-background-fetch';
 import { backgroundHeadlessTask } from './BackgroundJobs';
-PushNotification.configure({
-  onNotification: function (notification) {
-    console.log('NOTIFICATION:', notification);
-  },
-});
-BackgroundFetch.registerHeadlessTask(backgroundHeadlessTask);
+// PushNotification.configure({
+//   onNotification: function (notification) {
+//     console.log('NOTIFICATION:', notification);
+//   },
+// });
+// BackgroundFetch.registerHeadlessTask(backgroundHeadlessTask);
 
 AppRegistry.registerComponent(appName, () => App);
